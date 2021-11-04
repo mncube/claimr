2a0e4a88e86db1cc7da2e43e454f383242cd1f35
Mackson Ncube <macksonncube@gmail.com>
2021-11-04 09:48:09 -0700

    initial documentation and tests for relative_bias

--- R/relative_bias.R
+++ R/relative_bias.R
@@ -1,3 +1,34 @@
+#' Relative Bias
+#'
+#' Get the relative bias of a sample obtained from a claimr sampling function
+#'
+#' @param obj An object obtained from a claimr sampling function
+#' @param var A variable for the relative bias computation
+#' @param ... Extra arguments associated with the mean function
+#'
+#' @return A number obtained from computing the relative bias of var computed
+#' using the sample data frame and sample frame data frame from an object
+#' obtained from a claimr sampling function
+#' @export
+#'
+#' @examples
+#' #Generate sampling frame  with an index id from 1 to N
+#' df_sample_frame_num <- data.frame("sample_frame_sequence_id" = 1:1000,
+#'                                  "score" = rnorm(1000))
+#'
+#' #Get output and input in lists
+#' score_audit_num <- rs_singlestage(df = df_sample_frame_num,
+#'                                   seed_number = 100,
+#'                                   audit_review = "Score Audit",
+#'                                   quantity_to_generate = 100,
+#'                                   quantity_of_spares = 3,
+#'                                   frame_low = 1,
+#'                                   frame_high = 1000,
+#'                                   output_options = c("data.frame"))
+#'
+#' #Compute relative bias of sample stored in score_audit_num
+#'
+#' relative_bias(obj = score_audit_num, var = score)
 relative_bias <- function(obj = NULL, var = NULL, ...){
 
   #Get column name
---
+++ tests/testthat/test-relative_bias.R
@@ -0,0 +1,28 @@
+#Set up claimr sample object for testing
+#Generate sampling frame  with an index id from 1 to N
+df_sample_frame_num <- data.frame("sample_frame_sequence_id" = 1:1000,
+                                  "score" = rnorm(1000))
+
+#Get output and input in lists
+score_audit_num <- rs_singlestage(df = df_sample_frame_num,
+                                  seed_number = 100,
+                                  audit_review = "Score Audit",
+                                  quantity_to_generate = 100,
+                                  quantity_of_spares = 3,
+                                  frame_low = 1,
+                                  frame_high = 1000,
+                                  output_options = c("data.frame"))
+
+#Manually obtain relative bias of score_audit_num
+sample_mean <- mean(score_audit_num$output$output_options$sample$score)
+frame_mean <- mean(score_audit_num$output$output_options$sample_frame$score)
+manual_rel_bias <- abs((sample_mean - frame_mean)/frame_mean)
+
+#Use claimr's relative bias function to obtain relative bias of score_audit_num
+claimr_rel_bias <- relative_bias(obj = score_audit_num, var = score)
+
+
+test_that("Relative bias computed manually (manual_rel_bias) givs the same result
+          as the relative bias computed from claimr's relative_bias function", {
+  expect_equal(claimr_rel_bias, manual_rel_bias)
+})
